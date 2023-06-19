fb87f634ecca50768c9ce45892cc677f4c934ee1
tariknh <hajibtarik@gmail.com>
2023-06-19 23:45:21 +0200

    trying to fix deployment

--- app/locationWeather/page.tsx
+++ app/locationWeather/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import Link from "next/link"
-
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
